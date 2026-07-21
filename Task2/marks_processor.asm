0f7a96d8ebf53f35bbfde82348a9d912ba85840e
Ayub <jobb.roman@gmail.com>
2026-07-21 11:35:15 +0300

    Task 2 Version 4: Calculate average

--- Task2/marks_processor.asm
+++ Task2/marks_processor.asm
@@ -5,6 +5,9 @@ section .data
 
 marks db 0,39,40,55,69,70,85,100,62,47
 count equ 10
+highest db 0
+lowest  db 100
+average dq 0
 
 total dq 0
 
@@ -27,12 +30,38 @@ loop_marks:
     ; Add mark to total
     add rax, rbx
 
+    ; ----- Highest -----
+
+    cmp bl, [highest]
+    jle check_lowest
+
+    mov [highest], bl
+
+check_lowest:
+
+    ; ----- Lowest -----
+
+    cmp bl, [lowest]
+    jge continue_loop
+
+    mov [lowest], bl
+
+continue_loop:
+
     inc rcx
     jmp loop_marks
 
 done:
 
     mov [total], rax
+
+    ; ----- Average -----
+
+    xor rdx, rdx        ; Clear remainder register
+    mov rbx, count      ; Divisor = 10
+    div rbx             ; RAX = total / 10
+
+    mov [average], rax
 
     mov rax, 60
     xor rdi, rdi
